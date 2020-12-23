03bd0e0f7e1acb604e9aaaf05fd306940735d81a
주동민 <dongmin960903@gmail.com>
2020-12-23 19:52:39 +0900

    Add firebase library

--- app/src/main/java/in/hangang/hangang/MainActivity.kt
+++ app/src/main/java/in/hangang/hangang/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
