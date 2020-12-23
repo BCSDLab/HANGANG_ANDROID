24389a867bde77b033551ae5bcd3b99b940b0f92
주동민 <dongmin960903@gmail.com>
2020-12-23 18:12:33 +0900

    Add rxkotlin library

--- app/src/main/java/in/hangang/hangang/MainActivity.kt
+++ app/src/main/java/in/hangang/hangang/MainActivity.kt
@@ -1,12 +1,12 @@
 package `in`.hangang.hangang
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +22,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
+        
     }
 }
