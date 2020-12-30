49a3efb1f7eef681d0fe506189e618ab53105448
Seungmin Yang <tmdals099@gmail.com>
2020-12-31 00:48:40 +0900

    Added showSimpleDialog method in ActivityBase

--- core/src/main/java/in/hangang/core/ActivityBase.kt
+++ core/src/main/java/in/hangang/core/ActivityBase.kt
@@ -1,11 +1,36 @@
 package `in`.hangang.core
 
-import androidx.appcompat.app.AppCompatActivity
+import `in`.hangang.core.dialog.SimpleDialog
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 open class ActivityBase : AppCompatActivity() {
+    var simpleDialog: SimpleDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+    }
+
+    fun showSimpleDialog(
+        message: String,
+        positiveButtonText: String? = null,
+        negativeButtonString: String? = null,
+        positiveButtonOnClickListener: View.OnClickListener? = null,
+        negativeButtonOnClickListener: View.OnClickListener? = null
+    ) {
+        if (simpleDialog == null) {
+            simpleDialog = SimpleDialog(
+                this,
+                message,
+                positiveButtonText,
+                negativeButtonString,
+                positiveButtonOnClickListener,
+                negativeButtonOnClickListener
+            )
+
+            simpleDialog!!.show()
+        }
     }
 }
