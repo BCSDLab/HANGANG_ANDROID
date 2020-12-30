31aa3b34ef707239f9729d492ee8a334ecb49590
Seungmin Yang <tmdals099@gmail.com>
2020-12-31 00:19:15 +0900

    Added simple dialog(single message, positive, negative button) xml files

---
+++ core/src/main/java/in/hangang/core/dialog/SimpleDialog.kt
@@ -0,0 +1,9 @@
+package `in`.hangang.core.dialog
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+
+class SimpleDialog(context: Context) : Dialog(context) {
+    
+}
