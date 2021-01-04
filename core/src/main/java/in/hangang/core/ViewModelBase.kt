4802e9943a515dbfb52b7b766b7b755977d59ec3
Seungmin Yang <tmdals099@gmail.com>
2021-01-04 14:48:46 +0900

    Added BaseViewModel with CompositeDisposable

---
+++ core/src/main/java/in/hangang/core/ViewModelBase.kt
@@ -0,0 +1,20 @@
+package `in`.hangang.core
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+class ViewModelBase : ViewModel() {
+    val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(vararg disposables: Disposable) {
+        compositeDisposable.addAll(*disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if(!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+    }
+}
