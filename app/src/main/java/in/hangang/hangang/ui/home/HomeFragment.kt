c5a8dc2ce7378e4064ca7ad50b3acb3df0153784
Seungmin Yang <tmdals099@gmail.com>
2021-01-05 14:09:55 +0900

    Revert "Feature/add base class"

--- app/src/main/java/in/hangang/hangang/ui/home/HomeFragment.kt
+++ app/src/main/java/in/hangang/hangang/ui/home/HomeFragment.kt
@@ -1,12 +1,13 @@
 package `in`.hangang.hangang.ui.home
-
-import `in`.hangang.hangang.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import `in`.hangang.hangang.R
 class HomeFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater,
