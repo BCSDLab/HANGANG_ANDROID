028b24701c6fad043db25998562ab473e4a7858c
Seungmin Yang <tmdals099@gmail.com>
2021-01-06 15:44:01 +0900

    Changed MainActivity code to proper databinding code

--- app/src/main/java/in/hangang/hangang/MainActivity.kt
+++ app/src/main/java/in/hangang/hangang/MainActivity.kt
@@ -1,16 +1,18 @@
 package `in`.hangang.hangang
 
 import `in`.hangang.core.ActivityBase
+import `in`.hangang.hangang.R
 import `in`.hangang.hangang.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ActivityBase<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 }
