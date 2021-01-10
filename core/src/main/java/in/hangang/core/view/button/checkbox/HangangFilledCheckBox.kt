213611e36d4b51a91c53c063cb3e2d4b3a0c9e0c
Seungmin Yang <tmdals099@gmail.com>
2021-01-10 22:23:34 +0900

    Added Hangang CheckBox custom view

---
+++ core/src/main/java/in/hangang/core/view/button/checkbox/HangangFilledCheckBox.kt
@@ -0,0 +1,28 @@
+package `in`.hangang.core.view.button.checkbox
+
+import `in`.hangang.core.R
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.CheckBox
+import androidx.core.content.ContextCompat
+
+class HangangFilledCheckBox @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatCheckBox(context, attributeSet, defStyleAttr) {
+
+    init {
+        minWidth = resources.getDimensionPixelSize(R.dimen.radio_min_width)
+        gravity = Gravity.CENTER
+        isClickable = true
+        buttonDrawable = null
+        setBackgroundResource(R.drawable.rounded_corner_rectangle_filled)
+        backgroundTintList =
+            ContextCompat.getColorStateList(context, R.color.color_state_radio_button_blue)
+        setTextColor(
+            ContextCompat.getColorStateList(context, R.color.color_state_radio_button_text)
+        )
+    }
+}
--- core/src/main/java/in/hangang/core/view/button/radiobutton/HangangFilledRadioButton.kt
+++ core/src/main/java/in/hangang/core/view/button/radiobutton/HangangFilledRadioButton.kt
@@ -1,10 +1,9 @@
-package `in`.hangang.core.view.button
+package `in`.hangang.core.view.button.radiobutton
 
 import `in`.hangang.core.R
 import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
-import android.widget.RadioButton
 import androidx.core.content.ContextCompat
 
 class HangangFilledRadioButton @JvmOverloads constructor(
@@ -19,7 +18,10 @@ class HangangFilledRadioButton @JvmOverloads constructor(
         isClickable = true
         buttonDrawable = null
         setBackgroundResource(R.drawable.rounded_corner_rectangle_filled)
-        backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_state_radio_button_blue)
-        setTextColor(ContextCompat.getColorStateList(context, R.color.color_state_radio_button_text))
+        backgroundTintList =
+            ContextCompat.getColorStateList(context, R.color.color_state_radio_button_blue)
+        setTextColor(
+            ContextCompat.getColorStateList(context, R.color.color_state_radio_button_text)
+        )
     }
 }
