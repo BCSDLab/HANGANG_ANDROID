db4f583a6bc4164073537f854db16afb6be37f39
Seungmin Yang <tmdals099@gmail.com>
2021-01-10 22:11:19 +0900

    Added custom RadioButton

--- app/src/main/java/in/hangang/hangang/ui/home/HomeFragment.kt
+++ app/src/main/java/in/hangang/hangang/ui/home/HomeFragment.kt
@@ -1,8 +1,5 @@
 package `in`.hangang.hangang.ui.home
 
-import `in`.hangang.core.view.appbar.HangangAppBarClickListener
-import `in`.hangang.core.view.appbar.HangangProgressAppBar
-import `in`.hangang.hangang.LogUtil
 import `in`.hangang.hangang.R
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,37 +9,11 @@ import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val appBar = view.findViewById<HangangProgressAppBar>(R.id.app_bar)
-        appBar.hangangAppBarClickListener = object : HangangAppBarClickListener {
-            override fun onBackButtonClick(v: View) {
-                LogUtil.d("back button clicked")
-                activity?.finish()
-            }
-
-            override fun onRightButtonClick(v: View) {
-                LogUtil.d("Right button clicked")
-            }
-        }
-        var progress = 0
-        Thread {
-            while(progress <= appBar.max) {
-                progress += 1
-                activity?.runOnUiThread {
-                    appBar.progress = progress
-                }
-                Thread.sleep(100)
-            }
-
-        }.start()
     }
 }
---
+++ core/src/main/java/in/hangang/core/view/button/HangangRadioButton.kt
@@ -0,0 +1,25 @@
+package `in`.hangang.core.view.button
+
+import `in`.hangang.core.R
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import android.widget.RadioButton
+import androidx.core.content.ContextCompat
+
+class HangangRadioButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatRadioButton(context, attributeSet, defStyleAttr) {
+
+    init {
+        minWidth = resources.getDimensionPixelSize(R.dimen.radio_min_width)
+        gravity = Gravity.CENTER
+        isClickable = true
+        buttonDrawable = null
+        setBackgroundResource(R.drawable.rounded_corner_rectangle_filled)
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.color_state_radio_button_blue)
+        setTextColor(ContextCompat.getColorStateList(context, R.color.color_state_radio_button_text))
+    }
+}
