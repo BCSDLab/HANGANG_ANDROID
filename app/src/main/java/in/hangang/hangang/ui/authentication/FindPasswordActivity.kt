0687c3133cb0ce990f3a1458f4300e1f68eca040
Seungmin Yang <tmdals099@gmail.com>
2021-01-17 21:19:58 +0900

    Added empty activity and modified style parent to NoActionBar

---
+++ app/src/main/java/in/hangang/hangang/ui/authentication/FindPasswordActivity.kt
@@ -0,0 +1,11 @@
+package `in`.hangang.hangang.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password)
+    }
+}
