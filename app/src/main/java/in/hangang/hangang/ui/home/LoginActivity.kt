3a498ac108ff570ff034d5eb308a5bc997edf4a4
name <kso980212@gmail.com>
2021-01-19 21:36:53 +0900

    login layout

---
+++ app/src/main/java/in/hangang/hangang/ui/home/LoginActivity.kt
@@ -0,0 +1,21 @@
+package `in`.hangang.hangang.ui.home
+
+import `in`.hangang.hangang.R
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
