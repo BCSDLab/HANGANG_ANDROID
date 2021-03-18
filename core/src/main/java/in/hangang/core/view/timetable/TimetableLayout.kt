04f5def05cee8106bac342a53ff31950f0a31f89
Seungmin Yang <tmdals099@gmail.com>
2021-03-18 17:25:20 +0900

    Modified timetable view
    added custom row, column and removed header

--- core/src/main/java/in/hangang/core/view/timetable/TimetableLayout.kt
+++ core/src/main/java/in/hangang/core/view/timetable/TimetableLayout.kt
@@ -7,6 +7,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
+import android.icu.util.Measure
 import android.util.AttributeSet
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
@@ -18,7 +19,6 @@ class TimetableLayout @JvmOverloads constructor(context: Context, attrs: Attribu
 
     private val topBarHeight: Int by lazy { dp2Px(26f) }
 
-    private val bound = Rect()
 
     private val linePaint: Paint by lazy {
         Paint().apply {
@@ -26,18 +26,37 @@ class TimetableLayout @JvmOverloads constructor(context: Context, attrs: Attribu
         }
     }
 
+    private var cellWidth : Int = 0
+    private var cellHeight : Int = 0
+
     var rowCount = 9
+    set(value) {
+        field = value
+        postInvalidate()
+    }
     var columnCount = 5
+        set(value) {
+            field = value
+            postInvalidate()
+        }
 
     var rowHeight = 0
+        set(value) {
+            field = value
+            postInvalidate()
+        }
     var columnWidth = 0
+        set(value) {
+            field = value
+            postInvalidate()
+        }
 
     @ColorInt
     var dividerColor: Int = Color.LTGRAY
         set(value) {
             field = value
             linePaint.color = value
-            invalidate()
+            postInvalidate()
         }
 
     init {
@@ -60,64 +79,30 @@ class TimetableLayout @JvmOverloads constructor(context: Context, attrs: Attribu
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val defaultWidth = getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
-        val defaultHeight = getDefaultSize(suggestedMinimumHeight, heightMeasureSpec)
-
-        val maxHeight = rowCount * rowHeight
-        val maxWidth = columnCount * columnWidth
+        val defaultWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val defaultHeight = MeasureSpec.getSize(heightMeasureSpec)
 
         val width =
                 when (MeasureSpec.getMode(widthMeasureSpec)) {
-                    MeasureSpec.UNSPECIFIED -> {
-                        if (columnWidth < 0) {
-                            defaultWidth
-                        } else {
-                            maxWidth
-                        }
-                    }
-                    MeasureSpec.AT_MOST -> {
-                        if (maxWidth > defaultWidth) defaultWidth
-                        else maxWidth
-                    }
                     MeasureSpec.EXACTLY -> defaultWidth
-                    else -> defaultWidth
+                    else -> columnCount * columnWidth
                 }
 
         val height =
                 when (MeasureSpec.getMode(heightMeasureSpec)) {
-                    MeasureSpec.UNSPECIFIED -> {
-                        if (rowHeight < 0) {
-                            defaultHeight
-                        } else {
-                            maxHeight
-                        }
-                    }
-                    MeasureSpec.AT_MOST -> {
-                        if (maxHeight > defaultHeight) defaultHeight
-                        else maxHeight
-                    }
                     MeasureSpec.EXACTLY -> defaultHeight
-                    else -> defaultHeight
+                    else -> rowCount * rowHeight
                 }
 
-        val childDesiredWidthUnit = if (columnWidth < 0) {
-            width / columnCount
-        } else {
-            columnWidth
-        }
-
-        val childDesiredHeightUnit = if (rowHeight < 0) {
-            height / rowHeight
-        } else {
-            rowHeight
-        }
+        cellWidth = width / columnCount
+        cellHeight = height / rowCount
 
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             val lp = child.layoutParams as LayoutParams
 
-            val desiredWidth = childDesiredWidthUnit * (lp.columnEnd - lp.columnStart)
-            val desiredHeight = childDesiredHeightUnit * (lp.rowEnd - lp.rowStart)
+            val desiredWidth = cellWidth * (lp.columnEnd - lp.columnStart)
+            val desiredHeight = cellHeight * (lp.rowEnd - lp.rowStart)
 
             child.measure(
                     MeasureSpec.makeMeasureSpec(desiredWidth.toInt(), MeasureSpec.EXACTLY),
@@ -129,44 +114,38 @@ class TimetableLayout @JvmOverloads constructor(context: Context, attrs: Attribu
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val cellWidth = (measuredWidth / 11.0).roundToInt()
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             val lp = child.layoutParams as LayoutParams
 
-            val left = cellWidth + cellWidth * lp.weekday * 2
-            val right = cellWidth + cellWidth * (lp.weekday * 2 + 2)
-            val top = (topBarHeight + (lp.startTime - startTime) * timeHeight).roundToInt()
-            val bottom = (topBarHeight + (lp.endTime - startTime) * timeHeight).roundToInt()
+            val left = cellWidth * lp.columnStart
+            val right = cellWidth * lp.columnEnd
+            val top = cellHeight * lp.rowStart
+            val bottom = cellHeight * lp.rowEnd
 
-            child.layout(left, top, right, bottom)
+            child.layout(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
         }
     }
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.let {
-            val timeTextPaddingRight = dp2Px(6f)
-            val timeTextPaddingTop = dp2Px(4f)
-            val cellWidth = (measuredWidth / 11.0).roundToInt()
-            it.drawLine(0F, topBarHeight.toFloat(), measuredWidth.toFloat(), topBarHeight.toFloat(), linePaint)
-            for (i in 1..endTime - startTime + 1) {
-                val text = String.format("%02d", i + startTime - 1)
-                timeTextPaint.getTextBounds(text, 0, text.length, bound)
-                it.drawText(text, (cellWidth - timeTextPaddingRight - bound.width()).toFloat(), (topBarHeight + timeHeight * (i - 1) + bound.height() + timeTextPaddingTop).toFloat(), timeTextPaint)
-                it.drawLine(cellWidth.toFloat(), (topBarHeight + timeHeight * i).toFloat(), measuredWidth.toFloat(), (topBarHeight + timeHeight * i).toFloat(), linePaint)
+            for(i in 0..columnCount) {
+                it.drawLine(
+                        (cellWidth * i).toFloat(),
+                        0f,
+                        (cellWidth * i).toFloat(),
+                        measuredHeight.toFloat(),
+                        linePaint
+                )
             }
-            for (i in 0 until 5) {
-                val text = when (i) {
-                    MON -> context.getString(R.string.timetable_mon)
-                    TUE -> context.getString(R.string.timetable_tue)
-                    WED -> context.getString(R.string.timetable_wed)
-                    THU -> context.getString(R.string.timetable_thu)
-                    FRI -> context.getString(R.string.timetable_fri)
-                    else -> context.getString(R.string.timetable_mon)
-                }
-                weekdayTextPaint.getTextBounds(text, 0, text.length, bound)
-                it.drawText(text, (cellWidth * (3 + 2 * i) - cellWidth - bound.width() * 0.5).toFloat(), (topBarHeight.toFloat() - bound.height() * 0.5).toFloat(), weekdayTextPaint)
-                it.drawLine((cellWidth * (1 + 2 * i)).toFloat(), topBarHeight.toFloat(), (cellWidth * (1 + 2 * i)).toFloat(), measuredHeight.toFloat(), linePaint)
+            for(i in 0..rowCount) {
+                it.drawLine(
+                        0f,
+                        (cellHeight * i).toFloat(),
+                        measuredWidth.toFloat(),
+                        (cellHeight * i).toFloat(),
+                        linePaint
+                )
             }
         }
     }
