5188672fd8bceb7121bc29e23a4eb0e2cb2bcd79
Seungmin Yang <tmdals099@gmail.com>
2021-04-03 17:21:38 +0900

    Added mypage layout

---
+++ app/src/main/java/in/hangang/hangang/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,11 @@
+package `in`.hangang.hangang.ui.mypage
+
+import `in`.hangang.core.base.fragment.ViewBindingFragment
+import `in`.hangang.hangang.R
+import `in`.hangang.hangang.databinding.FragmentMyPageBinding
+
+class MyPageFragment : ViewBindingFragment<FragmentMyPageBinding>() {
+    override val layoutId = R.layout.fragment_my_page
+
+
+}
