c56131e592d3f371b02a1bce0f9e9ede182cb2ec
Seungmin Yang <tmdals099@gmail.com>
2021-04-28 11:15:17 +0900

    Fixed Parcelize annotation deprecated

--- app/src/main/java/in/hangang/hangang/data/entity/LectureFilter.kt
+++ app/src/main/java/in/hangang/hangang/data/entity/LectureFilter.kt
@@ -1,7 +1,7 @@
 package `in`.hangang.hangang.data.entity
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LectureFilter(
