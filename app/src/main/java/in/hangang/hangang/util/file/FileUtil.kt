d1a16a7345a37181afea329b6a8b09bcf9f9cfbc
Seungmin Yang <tmdals099@gmail.com>
2021-07-21 22:27:58 +0900

    Fixed critical timetable error

--- app/src/main/java/in/hangang/hangang/util/file/FileUtil.kt
+++ app/src/main/java/in/hangang/hangang/util/file/FileUtil.kt
@@ -8,6 +8,7 @@ import android.app.DownloadManager
 import android.content.*
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
@@ -25,7 +26,7 @@ class FileUtil(
         val contentValues = ContentValues().apply {
             put(MediaStore.Images.Media.DISPLAY_NAME, "$fileName.jpg")
             put(MediaStore.Images.Media.MIME_TYPE, "image/*jpeg")
-            requireQ {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 put(MediaStore.Images.Media.IS_PENDING, 1)
             }
         }
